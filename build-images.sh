b94060b8e542cbb82ac0fff4ec2b8c0d980b35b5
Ihor Okhrimenko <iokhrime@redhat.com>
2020-12-11 12:12:01 +0200

    Add deployment config for the front-end
    Signed-off-by: Ihor Okhrimenko <iokhrime@redhat.com>

--- build-images.sh
+++ build-images.sh
@@ -4,13 +4,19 @@
 docker build -t bujhtr5555/base-calculator-image:latest .
 
 
+docker build -t bujhtr5555/calc-front:latest -f my-app/Dockerfile my-app
+
+
 docker build -t bujhtr5555/calculator-server-div:latest -f server/div/Dockerfile server/div
 docker build -t bujhtr5555/calculator-server-mul:latest -f server/mul/Dockerfile server/mul
 docker build -t bujhtr5555/calculator-server-sub:latest -f server/sub/Dockerfile server/sub
 docker build -t bujhtr5555/calculator-server-sum:latest -f server/sum/Dockerfile server/sum
 
 
-docker login -u $1 -p $2
+docker login -u bujhtr5555 -p $1
+
+docker push bujhtr5555/calc-front:latest
+
 docker push bujhtr5555/calculator-server-div:latest
 docker push bujhtr5555/calculator-server-mul:latest
 docker push bujhtr5555/calculator-server-sub:latest
