0cbc8d38bdb3bfc755a576bb6b0f157cff43ed93
Ihor Okhrimenko <iokhrime@redhat.com>
2020-12-11 15:11:15 +0200

    Update images building
    Signed-off-by: Ihor Okhrimenko <iokhrime@redhat.com>

--- build-images.sh
+++ build-images.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 
-docker build -t bujhtr5555/base-calculator-image:latest .
+docker build -t quay.io/ohrimenko1988/base-calculator-image:latest .
 
 
-docker build -t bujhtr5555/calc-front:latest -f my-app/Dockerfile my-app
+docker build -t quay.io/ohrimenko1988/calc-front:latest -f my-app/Dockerfile my-app
 
 
-docker build -t bujhtr5555/calculator-server-div:latest -f server/div/Dockerfile server/div
-docker build -t bujhtr5555/calculator-server-mul:latest -f server/mul/Dockerfile server/mul
-docker build -t bujhtr5555/calculator-server-sub:latest -f server/sub/Dockerfile server/sub
-docker build -t bujhtr5555/calculator-server-sum:latest -f server/sum/Dockerfile server/sum
+docker build -t quay.io/ohrimenko1988/calculator-server-div:latest -f server/div/Dockerfile server/div
+docker build -t quay.io/ohrimenko1988/calculator-server-mul:latest -f server/mul/Dockerfile server/mul
+docker build -t quay.io/ohrimenko1988/calculator-server-sub:latest -f server/sub/Dockerfile server/sub
+docker build -t quay.io/ohrimenko1988/calculator-server-sum:latest -f server/sum/Dockerfile server/sum
 
 
-docker login -u bujhtr5555 -p $1
+docker login -u ohrimenko1988 -p $1 https://quay.io
 
-docker push bujhtr5555/calc-front:latest
+docker push quay.io/ohrimenko1988/calc-front:latest
 
-docker push bujhtr5555/calculator-server-div:latest
-docker push bujhtr5555/calculator-server-mul:latest
-docker push bujhtr5555/calculator-server-sub:latest
-docker push bujhtr5555/calculator-server-sum:latest
+docker push quay.io/ohrimenko1988/calculator-server-div:latest
+docker push quay.io/ohrimenko1988/calculator-server-mul:latest
+docker push quay.io/ohrimenko1988/calculator-server-sub:latest
+docker push quay.io/ohrimenko1988/calculator-server-sum:latest
 
 
 
